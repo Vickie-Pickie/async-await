561e8a2afa6a9f40c36cc3a7a6c2267a1d20ef93
viktoriamastrenko <victoriamastrenko@gmail.com>
2022-02-27 13:28:14 +0800

    changing in GameSavingLoader

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,10 +1,11 @@
 import json from './parser';
 import read from './reader';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
   static async load() {
     const buffer = await read();
     const value = await json(buffer);
-    return JSON.parse(value);
+    return new GameSaving(JSON.parse(value));
   }
 }
